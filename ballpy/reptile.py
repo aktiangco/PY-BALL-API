5c89c4a36426657cda02256ce929471e6b97910f
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-05-12 22:30:23 -0700

    added update and delete routes

--- ballpy/reptile.py
+++ ballpy/reptile.py
@@ -3,6 +3,7 @@ from . import models
 
 bp = Blueprint('reptile', __name__, url_prefix='/reptiles')
 
+# * INDEX PAGE
 @bp.route('/', methods=['GET', 'POST'])
 def index(): 
     #* POST method
@@ -38,7 +39,7 @@ def index():
 
     return reptile_dict # return the dictionary, which will get returned as JSON by default
 
-
+# * SHOW ID PAGE
 @bp.route('/<int:id>')
 def show(id): 
     reptile = models.User.query.filter_by(id=id).first() # find the reptile by id
@@ -52,3 +53,33 @@ def show(id):
     }
 
     return reptile_dict # return the dictionary, which will get returned as JSON by default
+
+# * Replace or Update ID page
+@bp.route('/<int:id>', methods=['PUT'])
+def update_reptile(id):
+    reptile = models.Reptile.query.get(id)  # find the reptile by id
+
+    if not reptile:
+        return {'message': 'Reptile not found.'}, 404
+
+    reptile.common_name = request.form.get('common_name', reptile.common_name)
+    reptile.scientific_name = request.form.get('scientific_name', reptile.scientific_name)
+    reptile.conservation_status = request.form.get('conservation_status', reptile.conservation_status)
+    reptile.native_habitat = request.form.get('native_habitat', reptile.native_habitat)
+    reptile.fun_fact = request.form.get('fun_fact', reptile.fun_fact)
+    models.db.session.commit()
+
+    return {'message': 'Reptile updated successfully.'}, 200
+
+
+# * Delete ID page
+@bp.route('/<int:id>', methods=['DELETE'])
+def delete_reptile(id):
+    reptile = models.Reptile.query.get(id)  # find the reptile by id
+
+    if reptile:
+        models.db.session.delete(reptile)
+        models.db.session.commit()
+        return {'message': 'Reptile deleted successfully.'}, 200
+    else:
+        return {'message': 'Reptile not found.'}, 404
